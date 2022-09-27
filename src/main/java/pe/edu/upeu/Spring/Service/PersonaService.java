035288d4014e0b3a4eb92a8231b5a59eb555d14a
F1RV <nikolai.cauper@gmail.com>
2022-09-26 19:10:25 -0500

    aaa

---
+++ src/main/java/pe/edu/upeu/Spring/Service/PersonaService.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package pe.edu.upeu.Spring.Service;
+
+import java.util.List;
+import pe.edu.upeu.Spring.entity.Persona;
+
+/**
+ *
+ * @author HP
+ */
+public interface PersonaService {
+    
+public List<Persona> findAll();
+
+public Persona findById( Long id);
+
+public Persona save (Persona persona);
+
+public void delete (Persona persona);
+
+public void deleteById (Long id);
+
+
+}
---
+++ src/main/java/pe/edu/upeu/Spring/repository/PersonaRepository.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package pe.edu.upeu.Spring.repository;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import pe.edu.upeu.Spring.entity.Persona;
+
+/**
+ *
+ * @author HP
+ */
+@Repository
+public interface PersonaRepository extends CrudRepository<Persona, Long>{
+
+    
+}
